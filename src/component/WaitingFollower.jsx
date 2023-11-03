322d285dfea7d85cd6691415b8d3af6ad62a63a6
Antonio Nattthan Krishna <92136335+natthankrish@users.noreply.github.com>
2023-11-04 00:11:14 +0700

    feat: followers page

---
+++ src/component/WaitingFollower.jsx
@@ -0,0 +1,76 @@
+import { 
+    Box,
+    Button,
+    Card, 
+    CardBody,
+    Divider,
+    Flex,
+    IconButton,
+    Image,
+    Modal,
+    ModalContent, 
+    ModalOverlay, 
+    ModalHeader, 
+    ModalCloseButton, 
+    useDisclosure, 
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverArrow,
+    Text,
+    Textarea,
+    CardFooter,
+    Heading} from "@chakra-ui/react"
+
+function Follower() {
+    return (
+      <>
+        <Card 
+            flex 
+            shadow={"2xl"} 
+            w={"30%"}
+            mb={"3%"}>
+            <CardBody>
+                <Flex
+                    flexDirection={"row"}
+                    gap={"5%"}
+                    justifyContent={"space-around"}>
+                    <Box
+                        width={"95%"}>
+                        <Heading as={"h2"}>Natthan Krish</Heading>
+                        <Text>@natthankrish</Text>
+                        <Flex
+                            m={"10px 0 5px 0"}
+                            flexDirection={"row"}>
+                            <Flex 
+                                flexDirection={"row"}
+                                alignItems={"center"}
+                                gap={"10px"}>
+                                <Image 
+                                    src="../../assets/check.png"
+                                    h={"20px"}
+                                    w={"20px"}/>
+                                <Text>120 Following</Text>
+                                <Image 
+                                    src="../../assets/date.png"
+                                    h={"20px"}
+                                    w={"20px"}/>
+                                <Text>12 Posts</Text>
+                            </Flex>
+                        </Flex>
+                        <Divider mt={"20px"}/>
+                        <Flex
+                            flexDirection={"row"}
+                            gap={"5%"}>
+                            <Button text={"Delete From Followers"} w={"100%"} >Approve</Button>
+                            <Button text={"Delete From Followers"} w={"100%"} >Delete</Button>
+                        </Flex>
+                    </Box>
+                </Flex>
+            </CardBody>
+        </Card>
+      </>
+    )
+}
+  
+export default Follower
--- src/page/Followers.jsx
+++ src/page/Followers.jsx
@@ -1,10 +1,102 @@
+import { 
+    Modal,
+    ModalContent, 
+    ModalOverlay, 
+    ModalHeader, 
+    ModalCloseButton, 
+    useDisclosure, 
+    Button, 
+    Input, 
+    Image, 
+    Textarea, 
+    Flex,
+    Box,
+    Heading,
+    ScaleFade,
+    Center} from '@chakra-ui/react'
+import Follower from '../component/Follower.jsx'
+import WaitingFollower from '../component/WaitingFollower.jsx'
 import Navbar from '../component/Navbar.jsx'
+import { Search2Icon } from '@chakra-ui/icons'
+import { useState } from 'react'
 
 function Followers() {
+    const [ isRequestPage, setRequestPage ] = useState(false)
+
+    function changeRequest() {
+        if (isRequestPage) {
+            document.getElementById('see-request-btn').innerText = "See Request"
+        } else {
+            document.getElementById('see-request-btn').innerText = "All Followers"
+        }
+        setRequestPage(!isRequestPage);
+    }
 
   return (
     <>
-      <Navbar />
+      <Flex
+        justify={"center"}
+        margin={"0 auto"}>
+        <Navbar />
+        <Box 
+            w={"80%"}
+            h={"100%"}
+            mt={"8%"}
+            justify={"center"}
+            flexDirection={"column"}>
+            <Flex
+                flexDirection={"row"}
+                justify={"space-between"}
+                align={"center"}
+                mb={"3%"}>
+                <Heading>Followers</Heading>
+                <Flex
+                    flexDirection={"row"}
+                    gap={"20px"}
+                    alignItems={"center"}>
+                    <Button p={"0 40px"} id="see-request-btn" onClick={changeRequest}>See Request</Button>
+                    <Search2Icon boxSize={7}/>
+                    <Input placeholder='Search ' size='lg' />
+                </Flex>
+            </Flex>
+            
+            {
+                isRequestPage ? 
+                    <Flex 
+                        flexDirection={"column"}
+                        background={"ButtonHighlight"}
+                        padding={"2%"}
+                        borderRadius={"55px"}>
+                        <Center 
+                            mb={"2%"}>
+                            <Heading>Request</Heading>
+                        </Center>
+                        <Flex
+                            justify={"left"}
+                            align={"center"}
+                            flexDirection={"row"}
+                            flexWrap={"wrap"}
+                            gap={"5%"} >
+                            <WaitingFollower />
+                            <WaitingFollower />
+                            <WaitingFollower />
+                            <WaitingFollower />
+                            <WaitingFollower />
+                            
+                        </Flex>
+                    </Flex>
+                :   <Flex
+                        justify={"left"}
+                        align={"center"}
+                        flexDirection={"row"}
+                        flexWrap={"wrap"}
+                        gap={"5%"}>
+                        <Follower />
+                        <Follower />
+                    </Flex>
+            }
+        </Box>
+      </Flex>
     </>
   )
 }
