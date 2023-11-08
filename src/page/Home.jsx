e65ea04232ef19a1af87acd875e02e5ceed91e84
Antonio Nattthan Krishna <92136335+natthankrish@users.noreply.github.com>
2023-11-08 12:08:32 +0700

    refactor: homepage styling

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,15 +1,35 @@
 import Navbar from '../component/Navbar.jsx';
-import '../style/HomePage.css';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 
 function Home() {
     return (
         <>
-            <Box className='wrapper'>
+            <Box     
+                h= {"100vh"}
+                w= {"100vw"}
+                backgroundImage={'../../assets/home-page.png'}
+                backgroundSize={'cover'}
+                backgroundRepeat={'no-repeat'}
+                backgroundPosition= {'center center'}
+                backgroundAttachment={'fixed'}
+                m={'0 auto'}>
                 <Navbar />
-                <Box className='hello-text'>
-                    <h1>Share Your Spaces Exclusively to Your Only Fans</h1>
-                </Box>
+                <Flex 
+                    position={'absolute'}
+                    height={'100%'}
+                    width={'100%'}
+                    justifyContent={'center'}
+                    alignItems= {'center'}>
+                    <Heading  
+                        margin= {0}
+                        padding= {0}
+                        maxWidth={'45vw'}
+                        fontSize={'60px'}
+                        fontWeight={'bold'}
+                        textAlign={'center'}>
+                            Share Your Spaces Exclusively to Your Only Fans
+                    </Heading>
+                </Flex>
             </Box>
         </>
     );
