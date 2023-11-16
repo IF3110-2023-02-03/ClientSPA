f7c5bd999d64280c5fb5ecd400698a5240d39b67
Antonio Nattthan Krishna <92136335+natthankrish@users.noreply.github.com>
2023-11-16 16:24:12 +0700

    feat: change profile

--- src/api/account.js
+++ src/api/account.js
@@ -14,3 +14,18 @@ export const updateUser = (username, fullname, description, pp_url) => axios.put
     'description': description,
     'pp_url': pp_url
 })
+
+export const changeProfile = (formData) => axios.post('http://localhost:3000/api/user/profile', formData, {
+    headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+        "Content-Type": "multipart/form-data"
+    }
+})
+
+export const getSource = (name) => axios.get('http://localhost:3000/api/user/profile/' + name, {
+    headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+    },
+    responseType: "blob"
+
+})
